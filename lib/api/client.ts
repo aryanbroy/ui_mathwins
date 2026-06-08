5f5a4dad3b67ce49f505da232d153d23f01110b8
136257977+aryanbroy@users.noreply.github.com <136257977+aryanbroy@users.noreply.github.com>
2026-06-09 00:41:00 +0530

    change baseurl

--- lib/api/client.ts
+++ lib/api/client.ts
@@ -3,6 +3,8 @@ import axios from 'axios';
 export const api = axios.create({
   // baseURL: 'http://10.130.85.225:3000/',
   // baseURL: 'http://192.168.29.2:3000/',
-  baseURL: 'https://mathwins.onrender.com/',
+  // baseURL: 'https://mathwins.onrender.com/',
   // baseURL: 'http://localhost:3000/',
+  baseURL: 'https://mathwins-api-yqx2voncyq-el.a.run.app/',
+
 });
