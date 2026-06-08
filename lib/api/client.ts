a5aeee5b21e8743037415cdf5c639794fe33ce9f
136257977+aryanbroy@users.noreply.github.com <136257977+aryanbroy@users.noreply.github.com>
2026-06-08 15:26:33 +0530

    use public backend url

--- lib/api/client.ts
+++ lib/api/client.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://10.130.85.225:3000/',
+  // baseURL: 'http://10.130.85.225:3000/',
   // baseURL: 'http://192.168.29.2:3000/',
-  // baseURL: 'https://mathwins.onrender.com/',
+  baseURL: 'https://mathwins.onrender.com/',
   // baseURL: 'http://localhost:3000/',
 });
